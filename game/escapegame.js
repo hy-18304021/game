72355b3688457b984cecc04e028b5e19d1f93a43
susu1996 <44827526+susu1996@users.noreply.github.com>
2019-07-11 10:59:39 +0900

    no message

--- game/escapegame.js
+++ game/escapegame.js
@@ -15,6 +15,7 @@
 	// バブリングを抑止
 	// この部分はoz-riddleさんのコードを参考にさせていただきました
 	// http://ozriddle.web.fc2.com/
+
 	function stopEvent( evt )
 	{
 		if( evt.stopPropagation ) evt.stopPropagation();
@@ -519,3 +520,45 @@
 	};
 
 };
+
+//TimeOut
+function timeOut(){
+		 function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+
+  return {
+    'total': t,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+	function isTime(){
+		alert("TimeOut");
+	}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+      isTime();
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) +  5 * 60 * 1000);
+initializeClock('clockdiv', deadline);
+}
